526d7f31b02bc538d459e6683a6c94adab19a2d1
NadiaKaputkina <nadia.kaputkina@gmail.com>
2019-09-26 23:35:33 +0300

    feat: add task Guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.value = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = Math.ceil((this.min + this.max)/2 );
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
